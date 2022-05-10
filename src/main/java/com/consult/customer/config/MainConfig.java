d7ed6d1c743bcf20a19f40e67c9f1fa31634e7ad
breiner.suarez <breiner.suarez@gtd.com.co>
2022-05-10 15:40:53 -0500

    Se quita importaciones que no se usa en las clases

--- src/main/java/com/consult/customer/config/MainConfig.java
+++ src/main/java/com/consult/customer/config/MainConfig.java
@@ -4,6 +4,7 @@ import org.springframework.boot.web.server.ErrorPageRegistrar;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
 @Configuration
 public class MainConfig {
 
